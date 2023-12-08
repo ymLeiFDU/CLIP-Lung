f8d140db1c8786122231f28a49085f5792a64ac8
ymLeiFDU <48376092+ymLeiFDU@users.noreply.github.com>
2023-12-08 10:22:10 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,47 @@
+python main.py \
+--model wideresnet \
+--lr 0.001 \
+--wd 1e-4 \
+--lambda-ce 1 \
+--lambda-kl 1 \
+--train-batch-size 32 \
+--gpu 4 \
+--seed 2 \
+--epochs 100 \
+--ablation CLIP-Lung-2class-softmax-weights \
+--dataset lidc \
+--num-labels full \
+--ema-alpha 0.999 \
+-ema \
+-lang \
+-save \
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
