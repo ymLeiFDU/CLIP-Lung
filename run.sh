fe1b40e5aa937fefe4a7148882658d0385f9f433
ymLeiFDU <48376092+ymLeiFDU@users.noreply.github.com>
2023-12-10 16:05:40 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,15 @@
 python main.py \
---model wideresnet \
+--model resnet \
 --lr 0.001 \
 --wd 1e-4 \
 --lambda-ce 1 \
---lambda-kl 1 \
+--lambda-contrast 1 \
 --train-batch-size 32 \
 --gpu 4 \
 --seed 2 \
 --epochs 100 \
 --ablation CLIP-Lung-2class \
 --dataset lidc \
---num-labels full \
 --ema-alpha 0.999 \
 -ema \
 -lang \
